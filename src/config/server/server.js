52b23fbfb4c7763c9ad645d953ba1e6bd3a90dd7
Igor Rodrigues <igor220597@hotmail.com>
2021-07-19 17:37:42 -0300

    some changes

--- src/config/server/server.js
+++ src/config/server/server.js
@@ -3,9 +3,6 @@ const customExpress = require('./customExpress');
 const app = customExpress();
 const port = process.env.PORT || 3000;
 
-//import db
-const database = require('../database/database');
-
 // import routes
 const routes = require('../../controllers/store-controllers');
 routes(app);
